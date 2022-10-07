6a09c3283b06231fcc4ee91472a58d10594c3d71
dmunofer <diego.fmz@hotmail.com>
2022-10-07 16:57:54 +0200

    catalogar modificada

--- Ejercicio5/catalogar.py
+++ Ejercicio5/catalogar.py
@@ -1,9 +1,17 @@
 from vehiculo import *
 from coche import *
 
-def catalogar(lista):
-    for elemento in lista:
-        print(type(elemento).__name__, elemento)
+def catalogar(lista, nruedas=-8):
+    cont=0
+    if nruedas ==-8:
+        for elemento in lista:
+            print(type(elemento).__name__, elemento)
+
+    elif Vehiculo.ruedas==nruedas:
+            cont+=1
+    else:
+        pass
+    return "Se han encontrado {} vehículo(s) con {} ruedas".format(cont,nruedas)
 
 vehiculo1= Vehiculo("rojo",6)
 vehiculos=[coche,coche2,vehiculo1]
