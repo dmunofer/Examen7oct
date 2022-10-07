190b9439a4777c20f0c39d6be2466efefd59213b
dmunofer <diego.fmz@hotmail.com>
2022-10-07 16:31:28 +0200

    lista vehiculos y main. fichero coche arreglado

---
+++ Ejercicio5/coche.py
@@ -0,0 +1,15 @@
+from vehiculo import *
+class Coche(Vehiculo):
+
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return "color {}, {} km/h, {} ruedas, {} cc".format( self.color, self.velocidad, self.ruedas, self.cilindrada )
+
+coche = Coche("azul", 150, 4, 1200)
+coche2= Coche("negro", 180, 5, 1500)
+print(coche)
---
+++ Ejercicio5/main.py
@@ -0,0 +1,9 @@
+from vehiculo import *
+from coche import Coche
+
+coche = Coche("azul", 150, 4, 1200)
+coche2= Coche("negro", 180, 5, 1500)
+vehiculo1= Vehiculo("rojo",6)
+
+vehiculos=[coche,coche2,vehiculo1]
+
