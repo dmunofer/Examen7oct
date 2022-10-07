54f97052a33644cb8e0e316b500b1018ecdd2aaf
dmunofer <diego.fmz@hotmail.com>
2022-10-07 18:59:14 +0200

    terminado

--- Ejercicio5/main.py
+++ Ejercicio5/main.py
@@ -2,8 +2,8 @@ from vehiculo import *
 from coche import *
 import catalogar
 
-coche = Coche("azul", 150, 4, 1200)
-coche2= Coche("negro", 180, 5, 1500)
+coche = Coche("azul", 4, 150, 1200)
+coche2= Coche("negro", 5, 180, 1500)
 vehiculo1= Vehiculo("rojo",6)
 
 vehiculos=[coche,coche2,vehiculo1]
