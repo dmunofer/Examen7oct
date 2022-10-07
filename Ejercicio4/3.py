c097adf5122769e2da1840f679a7fa94d9e24a6d
dmunofer <diego.fmz@hotmail.com>
2022-10-07 17:08:12 +0200

    error 3 funciona

---
+++ Ejercicio4/3.py
@@ -0,0 +1,8 @@
+def error3(dict,clave):
+    if clave not in dict:
+        raise Exception('La clave no se encuentra en el diccionario')
+    else:
+        return dict[clave]
+
+paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" }
+
