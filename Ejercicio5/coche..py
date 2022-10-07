69f8581036758556fb270fb415eee45a72f007aa
dmunofer <diego.fmz@hotmail.com>
2022-10-07 16:26:39 +0200

    ahora funciona

--- Ejercicio5/coche..py
+++ Ejercicio5/coche..py
@@ -1,18 +1,13 @@
-from vehiculo import Vehiculo
+from vehiculo import *
 class Coche(Vehiculo):
 
     def __init__(self, color, ruedas, velocidad, cilindrada):
-
         self.color = color
-
         self.ruedas = ruedas
-
         self.velocidad = velocidad
-
         self.cilindrada = cilindrada
 
     def __str__(self):
-
         return "color {}, {} km/h, {} ruedas, {} cc".format( self.color, self.velocidad, self.ruedas, self.cilindrada )
 
 coche = Coche("azul", 150, 4, 1200)
