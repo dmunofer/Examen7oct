4df22ec89adf6249c44f678978fe645c52a5749f
dmunofer <diego.fmz@hotmail.com>
2022-10-07 18:56:28 +0200

    tests catalogar modificada pasados funciona todo

--- Ejercicio5/catalogar.py
+++ Ejercicio5/catalogar.py
@@ -1,20 +1,23 @@
 from vehiculo import *
 from coche import *
 
+
+
 def catalogar(lista, nruedas=-8):
     coincidenlasruedas=0
-    if nruedas ==-8:
+    if nruedas==-8:
         for elemento in lista:
             print(type(elemento).__name__, elemento)
-
-    elif Vehiculo.getruedas==nruedas:
-            coincidenlasruedas+=1
-    elif Coche.getruedas==nruedas:
-            coincidenlasruedas+=1
     else:
-        pass
+        for elemento in lista:
+            if elemento.getruedas()==nruedas:
+                coincidenlasruedas+=1
 
+            else:
+                pass
         return "Se han encontrado {} vehículo(s) con {} ruedas".format(coincidenlasruedas,nruedas)
 
 vehiculo1= Vehiculo("rojo",6)
-vehiculos=[coche,coche2,vehiculo1]
+coche = Coche("azul", 4, 150, 1200)
+coche2= Coche("negro", 5, 140, 1500)
+vehiculos=[coche, coche2, vehiculo1]
